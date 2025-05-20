dd83210889d38c8c5a52d6223b7af3d258cf458d
Thai Tran <1tranquocthai@gmail.com>
2025-05-20 17:43:51 +1000

    Update

--- optimize/result.py
+++ optimize/result.py
@@ -1,3 +1,4 @@
+from typing import List
 import numpy as np
 
 class OptimizeResult:
@@ -11,9 +12,11 @@ class OptimizeResult:
         message (str): A message describing the outcome of the optimization.
     """
 
-    def __init__(self, x: np.ndarray, fun: float, success: bool, message: str, nit: int):
+    def __init__(self, x: np.ndarray, fun: float, success: bool, message: str, nit: int, values: List[float], path: List[np.ndarray]):
         self.x = x
         self.fun = fun
         self.success = success
         self.message = message
         self.nit = nit
+        self.values = values
+        self.path = path
--- optimize/steepest_descent.py
+++ optimize/steepest_descent.py
@@ -4,7 +4,7 @@ from scipy.optimize import line_search
 from .result import OptimizeResult
 
 def steepest_descent(
-    fun, x0: np.ndarray, jac, ls=line_search, maxiter=100, amax=1000.0, tol=1.0e-8
+    fun, x0: np.ndarray, jac, ls=line_search, maxiter=100, amax=1000.0, tol=1.0e-8, optional_line_search=None
 ):
     x_eps = tol  # tolerence for convergence on delta x
     f_eps = tol  # tolerence for convergence on delta f
@@ -13,22 +13,29 @@ def steepest_descent(
     nit = 1
     f_k = fun(x_k)
     d_k = -jac(x_k)
+    values = []
+    path = []
     if la.norm(jac(x_k)) < g_eps:
-        return OptimizeResult(x_k, f_k, True, "norm of gradient is within tolerence", 0)
+        return OptimizeResult(x_k, f_k, True, "norm of gradient is within tolerence", 0, [f_k], [x_k])
     while True:
+        path.append(x_k.copy())
+        values.append(f_k)
         alpha_k, _, _, _, _, success = ls(fun, jac, x_k, d_k, amax=amax)
         if success is None:
-            raise Exception("Line search failed, change line search method")
+            if optional_line_search is not None:
+                alpha_k = optional_line_search(fun, jac, x_k, d_k, amax=amax)
+            else:
+                raise Exception("Line search failed, change line search method")
         if abs(alpha_k * la.norm(jac(x_k))) < x_eps:
-            return OptimizeResult(x_k, f_k, True, "change of x is within tolerence", nit)
+            return OptimizeResult(x_k, f_k, True, "change of x is within tolerence", nit, values, path)
         x_k1 = x_k + alpha_k * d_k
         d_k1 = -jac(x_k1)
         if abs(f_k - fun(x_k1)) < f_eps:
-            return OptimizeResult(x_k, f_k, True, "change of fun is within tolerence", nit)
+            return OptimizeResult(x_k, f_k, True, "change of fun is within tolerence", nit, values, path)
         if la.norm(jac(x_k1)) < g_eps:
-            return OptimizeResult(x_k, f_k, True, "norm of gradient is within tolerence", nit)
+            return OptimizeResult(x_k, f_k, True, "norm of gradient is within tolerence", nit, values, path)
         if nit > maxiter:
-            return OptimizeResult(x_k, f_k, False, "Max iter reached", nit)
+            return OptimizeResult(x_k, f_k, False, "Max iter reached", nit, values, path)
         nit += 1
         x_k = x_k1
         f_k = fun(x_k1)
